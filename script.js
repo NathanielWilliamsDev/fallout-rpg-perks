a7ebd2bb968498caa6d2023c685554b3f71a200a
Nathaniel <nathaniel.c.williams@hotmail.com>
2024-05-25 21:44:14 +1000

    Updated the functionality for opening and closing a cardItem, clicking the 'x' in the corner closes the card rather than clicking on the card itself. Adjusted the HTML structure for each perk to allow this functionality to work throughout the entire web app

--- script.js
+++ script.js
@@ -1,20 +1,68 @@
-function test(div){ // click
-    let originalDiv = div;
-    let cloneDiv = originalDiv.cloneNode(true); // Creating a clone of the clicked on div
-    let wrapper = document.querySelector(".cardContainer");
-    // If there is an instance of a cloned div (div with .delete class), remove it
-    if(document.querySelector(".delete") !== null){ 
-        let duplicate = document.querySelector(".delete")
-        duplicate.remove();
-    }
-    // if cloned div doesn't have .selected
-    if(!cloneDiv.classList.contains("selected")){
-        cloneDiv.classList.add("selected"); // add it
-        cloneDiv.classList.remove("cardItem"); // remove the default .cardItem
-        cloneDiv.classList.add("delete"); // give it a class to look out for late (for removal)
-        wrapper.appendChild(cloneDiv); // append it to the wrapper
-    }
+// function test(div){ // click
+//     console.log(div);
+//     let originalDiv = div;
+//     let cloneDiv = originalDiv.cloneNode(true); // Creating a clone of the clicked on div
+//     let wrapper = document.querySelector(".cardContainer");
+//     // If there is an instance of a cloned div (div with .delete class), remove it
+//     if(document.querySelector(".delete") !== null){ 
+//         let duplicate = document.querySelector(".delete")
+//         duplicate.remove();
+//     }
+//     // if cloned div doesn't have .selected
+//     if(!cloneDiv.classList.contains("selected")){
+//         cloneDiv.classList.add("selected"); // add it
+//         cloneDiv.classList.remove("cardItem"); // remove the default .cardItem
+//         cloneDiv.classList.add("delete"); // give it a class to look out for later (for removal)
+//         wrapper.appendChild(cloneDiv); // append it to the wrapper
+//     }
+// }
+
+
+
+// function test(div){
+//     div.classList.add("selected");
+//     div.classList.remove("cardItem");
+//     let selected = document.querySelector(".selected");
+//     let img = document.querySelector(".selected img");
+//     let closeContainer = document.createElement("div");
+//     let close = document.createElement("p");
+//     closeContainer.appendChild(close);
+//     close.textContent = "x";
+//     closeContainer.classList.add("closeDiv");
+//     close.classList.add("close")
+//     selected.insertBefore(closeContainer, img);
+    
+//     closeContainer.addEventListener("click", () =>{
+//         div.classList.add("cardItem");
+//         div.classList.remove("selected");
+//     });
+// }
+
+
+
+const buttons = document.querySelectorAll(".button");
+
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", (e)=>{
+        console.log(buttons[i]);
+        buttons[i].classList.add("selected");
+        buttons[i].classList.remove("cardItem");
+
+        const close = buttons[i].querySelector(".close");
+        close.style.display = "flex";
+
+        close.addEventListener("click", () =>{
+            setTimeout(()=>{
+                buttons[i].classList.remove("selected");
+                buttons[i].classList.add("cardItem");
+                close.style.display = "none";
+            }, 10);
+        }, {once: true});
+    });
 }
+
+
+
 
 let darkModeTriggered = false;
 function darkMode(){
